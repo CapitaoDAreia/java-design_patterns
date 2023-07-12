37938d98deb956bb8d4b283a5aac5f9b8fe73d53
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-12 20:26:53 -0300

    FACTORY METHOD: Half Simple Implementation Analysis

--- creational/creational-patterns-ref/src/main/java/br/com/cod3r/factory/apple/halfSimple/Client.java
+++ creational/creational-patterns-ref/src/main/java/br/com/cod3r/factory/apple/halfSimple/Client.java
@@ -5,6 +5,17 @@ import br.com.cod3r.factory.apple.halfSimple.factory.IPhoneFactory;
 import br.com.cod3r.factory.apple.halfSimple.factory.IPhoneXFactory;
 import br.com.cod3r.factory.apple.halfSimple.model.IPhone;
 
+// Todo: Factory Review
+
+/*
+	Nessa implementação, as fábricas recebem parâmetros e decidem com base nesses parâmetros qual será
+	o objeto retornado. Perceba que tudo continua da mesma forma. Existe uma classe Factory na qual as
+	Factories mais específicas se baseiam decidindo com base em parâmetros qual será a criação retornada.
+
+	Clicando dentro das Factories nós percebemos que uma certa complexidade foi adicionada a elas, pois agora
+	precisam lidar com parâmetros para efetuar seus retornos.
+*/
+
 public class Client {
 	
 	public static void main(String[] args) {
