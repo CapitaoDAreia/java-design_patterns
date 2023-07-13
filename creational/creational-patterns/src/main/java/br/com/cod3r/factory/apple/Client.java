8c14f53dd06542fdbbc3517b99edf076982f4a68
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-12 21:09:36 -0300

    FACTORY METHOD: Implementing Simple Factory Pattern

--- creational/creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ creational/creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,18 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public class Client {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("### Ordering an iPhone X");
-		
+		IPhone iphone = IPhoneFactory.orderIphone("11", "standard");
+		System.out.println(iphone);
+
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		IPhone iphone2 = IPhoneFactory.orderIphone("X", "highEnd");
+		System.out.println(iphone2);
 	}
 }
--- creational/creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
+++ creational/creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -1,6 +1,43 @@
 package br.com.cod3r.factory.apple.factory;
 
+import br.com.cod3r.factory.apple.model.IPhone;
+import br.com.cod3r.factory.apple.model.IPhone11;
+import br.com.cod3r.factory.apple.model.IPhone11Pro;
+import br.com.cod3r.factory.apple.model.IPhoneX;
+import br.com.cod3r.factory.apple.model.IPhoneXSMax;
+
 public abstract class IPhoneFactory {
 
-	
+	public static IPhone orderIphone(String generation, String level){
+        IPhone device = null;
+
+        if("X".equals(generation)){
+            if("standard".equals(level)){
+                device = new IPhoneX();
+            }
+
+            if("highEnd".equals(level)){
+                device = new IPhoneXSMax();
+            }
+        }
+
+        if("11".equals(generation)){
+            if("standard".equals(level)){
+                device = new IPhone11();
+            }
+
+            if("highEnd".equals(level)){
+                device = new IPhone11Pro();
+            }
+        }
+
+        if(device != null){
+            device.getHardware();
+            device.assemble();
+            device.certificates();
+            device.pack();
+        }
+
+        return device;
+    };
 }
