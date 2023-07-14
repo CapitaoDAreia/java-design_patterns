bf36329a983b9f46be8fc1aef25d6c5670f3cbfd
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-14 16:14:07 -0300

    SINGLETON: Implementation Analysis

--- creational/creational-patterns/src/main/java/br/com/cod3r/singleton/connectionPool/Client.java
+++ creational/creational-patterns/src/main/java/br/com/cod3r/singleton/connectionPool/Client.java
@@ -3,6 +3,17 @@ package br.com.cod3r.singleton.connectionPool;
 import br.com.cod3r.singleton.connectionPool.conn.Connection;
 import br.com.cod3r.singleton.connectionPool.conn.ConnectionPool;
 
+// TODO: Singleton review
+/*
+	- A intenção do padrão singleton é que uma determinada classe possua somente uma instância 
+	em um processo, e essa classe forneça um método estático para o acesso dessa instância.
+	- No modelo abaixo, como podemos ver, a classe 'ConnectionPool' possui seu construtor privado,
+	ou seja, ele só pode ser acessado dentro da própria classe. A instanciação dessa classe ocorre
+	dentro dela mesma, e para compartilhar essa instância há um método estático (pertence somente à classe, 
+	não a um objeto da classe) que retorna a instância dessa classe, onde é devolvida uma pool de conexões.
+
+*/
+
 public class Client {
 	public static void doQuery1() {
 		ConnectionPool pool = new ConnectionPool();
