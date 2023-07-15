f98db72eecb94a5e73f2b60767e0ee8f7c0cfa16
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-15 17:49:00 -0300

    PROTOTYPE: Implementation Analysis, shallow and deep copy

--- creational/creational-patterns/src/main/java/br/com/cod3r/prototype/deepShallow/Client.java
+++ creational/creational-patterns/src/main/java/br/com/cod3r/prototype/deepShallow/Client.java
@@ -3,6 +3,20 @@ package br.com.cod3r.prototype.deepShallow;
 import br.com.cod3r.prototype.deepShallow.model.Address;
 import br.com.cod3r.prototype.deepShallow.model.User;
 
+// TODO: Prototype review
+/*
+	- A intentção do padrão Prototype é especificar os tipos de objetos a serem criados usando
+	uma instância-protótipo e criar novos objetos pela cópia desse protótipo.
+	- No caso em questão, o objeto user é clonado no trecho 'User cloneUser = user.clone()',
+	fazendo o uso de um recurso do próprio Java para este tipo de operação.
+	- Vale a pena destacar que existem dois tipos de cópia de objetos. a deep copy e a shallow copy.
+	- Shallow copy se refere a uma cópia mais superficial dos objetos, outros objetos que integrarem o objeto
+	copiado neste modelo terão suas referências compartilhadas ao invés de clonadas.
+	- No caso da deep copy, o que acontecerá é o clone de todos os objetos que integrarem o objeto copiado.
+	Neste modelo, os objetos integrantes devem implementar a interface 'Cloneabe' afim de usar o método
+	nativo 'clone'. 
+*/
+
 public class Client {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
