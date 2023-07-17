0dd978f9d9e49ca7d9f466066bd3ff531b6926a9
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-17 20:16:17 -0300

    ADAPTER: Implementation and Analysis

--- estructural/estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
+++ estructural/estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
@@ -1,7 +1,20 @@
 package br.com.cod3r.adapter.tvPort;
 
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToVGAAdapter;
 import br.com.cod3r.adapter.tvPort.devices.Computer;
+import br.com.cod3r.adapter.tvPort.devices.OldMonitor;
 import br.com.cod3r.adapter.tvPort.devices.TV;
+
+// TODO: Adapter review
+/*
+	O padrão Adapter tem como intenção permitir que duas classes que implementam
+	interfaces diferentes trabalhem em conjunto.
+	- Podemos ver isso no exemplo abaixo de forma simples, tomando como base
+	um monitor que possui uma entrada VGA sendo conectado em um PC que possui uma
+	saída HDMI. Para que isso seja possível, temos um Adapter 'HDMIToVGAAdapter',
+	que recebe uma conexão VGA e a devolve para a entrada HDMI, fazendo a mediação
+	entre as duas interfaces distintas.
+*/
 
 public class Client {
 
@@ -12,11 +25,12 @@ public class Client {
 		pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 		
 		System.out.println("------ Monitor ----------");
-		
-
+		OldMonitor monitor = new OldMonitor();
+		Computer pc2 = new Computer();
+		HDMIToVGAAdapter monitorAdapter =  new HDMIToVGAAdapter(monitor);
+		pc2.connectPort(monitorAdapter);
 
 		System.out.println("------ Monitor Class Adapter ----------");
 		
-
 	}
 }
--- estructural/estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/adapters/HDMIToVGAAdapter.java
+++ estructural/estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/adapters/HDMIToVGAAdapter.java
@@ -1,5 +1,22 @@
 package br.com.cod3r.adapter.tvPort.adapters;
 
-public class HDMIToVGAAdapter {
+import br.com.cod3r.adapter.tvPort.devices.OldMonitor;
+import br.com.cod3r.adapter.tvPort.intefaces.HDMI;
 
+public class HDMIToVGAAdapter implements HDMI {
+    OldMonitor monitor = null;
+
+    public HDMIToVGAAdapter(OldMonitor monitor){
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void setImage(String image) {
+        monitor.setImage(image);
+    }
+
+    @Override
+    public void setSound(String sound) {
+        System.out.println("Sound is not supported on VGA monitors | sound: " + sound);
+    }
 }
