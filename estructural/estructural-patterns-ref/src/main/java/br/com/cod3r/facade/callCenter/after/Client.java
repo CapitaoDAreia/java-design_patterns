bcd8b439075e9180522ce1608341dc6f052c64cd
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-18 20:39:24 -0300

    FACADE: Implementation Analysis

--- estructural/estructural-patterns-ref/src/main/java/br/com/cod3r/facade/callCenter/after/Client.java
+++ estructural/estructural-patterns-ref/src/main/java/br/com/cod3r/facade/callCenter/after/Client.java
@@ -3,6 +3,15 @@ package br.com.cod3r.facade.callCenter.after;
 import br.com.cod3r.facade.callCenter.after.facade.CallCenterFacade;
 import br.com.cod3r.facade.callCenter.after.model.Card;
 
+//TODO: review Facade
+/*
+	A intenção do padrão Facade é oferecer uma interface única para um conjunto de interfaces de um subsistema.
+	- Isso é observável no exemplo abaixo, onde temos um conjunto de serviços diferentes que são todos
+	acessados através de uma Facade 'CallCenterFacade'. Isso nos ajuda a reduzir o nível de acoplamento e a promover
+	um nível de encapsulamento. A probabilidade de sofrimento para o cliente é menor caso algo mude nos serviços
+	requisitados.
+*/
+
 public class Client {
 
 	public static void main(String[] args) {
