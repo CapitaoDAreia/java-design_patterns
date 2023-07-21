ee5cacf56fa117a2baa8094618e69e0e95e297d5
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-21 14:54:28 -0300

    PROXY: Implementation Analysis

--- estructural/estructural-patterns-ref/src/main/java/br/com/cod3r/proxy/bank/Client.java
+++ estructural/estructural-patterns-ref/src/main/java/br/com/cod3r/proxy/bank/Client.java
@@ -4,6 +4,16 @@ import br.com.cod3r.proxy.bank.services.ATM;
 import br.com.cod3r.proxy.bank.services.Bank;
 import br.com.cod3r.proxy.bank.services.BankOperations;
 
+//TODO: review Proxy
+/*
+	- A intenção do padrão Proxy é prover um substituto ou ponto através
+	do qual um objeto possa controlar o acesso a outro objeto.
+	- No caso em questão, o objeto acessado é o 'Bank', e o proxy, controlador, é
+	o objeto 'ATM'. A implementação do Proxy pode variar de acordo com seu objetivo.
+	Por exemplo, um Remote Proxy controla o acesso a obejetos remotos. Já o Smart Proxy
+	possui como objetivo a adição/modificação de funcionalidades, além do controle de acesso.
+*/
+
 public class Client {
 
 	public static void main(String[] args) {
