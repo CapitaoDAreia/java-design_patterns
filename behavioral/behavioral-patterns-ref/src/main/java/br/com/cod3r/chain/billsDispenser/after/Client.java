9e174623d9b28ea13a5ff77128adf89e5595aee4
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-23 13:19:21 -0300

    CHAIN OF RESPONSIBILITY: Implementation Analysis

--- behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/chain/billsDispenser/after/Client.java
+++ behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/chain/billsDispenser/after/Client.java
@@ -1,5 +1,18 @@
 package br.com.cod3r.chain.billsDispenser.after;
 
+// TODO: review Chain Of Responsability
+/*
+	- A intenção do padrão CoR é fornecer uma cadeia de objetos para tratar uma solicitação, evitando
+	o acoplamento entre o solicitante e o objeto que irá tratar essa solicitação.
+	- No caso abaixo, temos um exemplo disso simulando a dispensa de cédulas em um caixa eletrônico.
+	Recebe-se um valor, trata-se esse valor em cédulas maiores, definindo quantas das cédulas maiores
+	contemplam o valor recebido. Em seguida, chama-se um método que fará a mesma coisa com o valor
+	restante relacionando-o a cédulas menores, e assim sucessivamente, até que o valor seja completamente
+	contemplado pelas cédulas disponíveis.
+	- O padrão CoR, neste caso, está presente no compartilhamento de responsabilidades em cadeia, até que determinada
+	solicitação seja atendida pelo receptor, que neste caso, é o método 'withDraw' do objeto 'dispenser'. 
+*/
+
 public class Client {
 
 	public static void main(String[] args) {
