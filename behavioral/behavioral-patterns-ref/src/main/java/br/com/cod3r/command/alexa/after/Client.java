b2f4b4c04e782ee275114a1760e7f07e98f2e91a
CapitaoDAreia <danieligord7@hotmail.com>
2023-07-26 16:54:04 -0300

    COMMAND: Implementation Analysis

--- behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/command/alexa/after/Client.java
+++ behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/command/alexa/after/Client.java
@@ -6,6 +6,21 @@ import br.com.cod3r.command.alexa.after.commands.TurnLightOnCommand;
 import br.com.cod3r.command.alexa.after.lights.PhillipsHueLight;
 import br.com.cod3r.command.alexa.after.lights.XiaomiLight;
 
+// TODO: review COMMAND
+/*
+	A intenção do padrão command é encapsular uma solicitação como um objeto, desta forma, será possível parametrizar
+	clientes com diferentes solicitações, enfileirando ou fazendo registros(logs).
+	- No caso em questão, temos  como exemplo uma simulação do que seria a Alexa, da Amazon.
+	As integrações com luzes elétricas são adicionadas na alexa, e para isso, utilizamos uma key, um comando e uma lista de keywords.
+	A key representa o comando em si, e as keywords são formas de identificar o comando em uma frase específica.
+	Quando fazemos uma request que possuam as keywords, o comando é disparado. 
+
+	- Certo, mas onde está o padrão?
+	Neste caso, o padrão command se materializa quando temos uma interface que espera um comando genérico, e assim podemos
+	passar o comando que desejarmos para qualquer tipo de integração que for feita, não importando se é uma luz da marca x ou y.
+	Dessa forma, tratamos as solicitações como objetos.
+*/
+
 public class Client {
 
 	public static void configureAlexa(Alexa alexa) {
