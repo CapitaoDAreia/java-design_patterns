88c99bdf30bd74defa9c8fdb0ec339b55cd57ade
CapitaoDAreia <danieligord7@hotmail.com>
2023-08-02 16:28:42 -0300

    STATE: Implementation Analysis

--- behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/state/headphone/after/Client.java
+++ behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/state/headphone/after/Client.java
@@ -1,5 +1,16 @@
 package br.com.cod3r.state.headphone.after;
 
+//Todo: review Padrão State
+/*
+	A intenção do Padrão State é permitir a um objeto alterar o seu comportamento quando o seu estado
+	interno mudar. O objeto irá aparentar mudar de classe.
+	- No caso em questão, temos o exemplo de um HeadPhone. Esse HeadPhone possui os estados desligado, ligado e tocando.
+	O que acontece é que o cliente faz a alteração desses estados e, quando essa alteração acontece,
+	uma nova instância de singleton é adicionada a essa classe HeadPhone, fazendo com que, apesar de seus 
+	métodos permanecerem o mesmo, suas implementações mudem de acordo com cada instância, já que as 
+	diferentes instâncias representam diferentes estados.
+*/
+
 public class Client {
 
 	public static void main(String[] args) {
