1fa24f31d4caa86f7f7486b1343bb168f368d775
CapitaoDAreia <danieligord7@hotmail.com>
2023-08-02 16:46:07 -0300

    STRATEGY: Implementation Analysis

--- behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/strategy/calc/after/Client.java
+++ behavioral/behavioral-patterns-ref/src/main/java/br/com/cod3r/strategy/calc/after/Client.java
@@ -5,6 +5,16 @@ import br.com.cod3r.strategy.calc.after.strategies.DivisionOperation;
 import br.com.cod3r.strategy.calc.after.strategies.MultiplicationOperation;
 import br.com.cod3r.strategy.calc.after.strategies.SubtractionOperation;
 
+//Todo: review Padrão Strategy
+/*
+	A intenção do Padrão Strategy é encapsular uma família de algoritmos em objetos permitindo
+	que possam ser utilizados de forma intercambiável, ou seja, o algoritmo específico pode
+	ser trocado sem que o módulo usuário desse algoritmo precise ser alterado.
+	- Neste exemplo prático isso acontece quando, para retornar o resultado de uma determinada operação,
+	já possuindo os valores, a objeto calc recebe somente o algoritmo para efetuar o calculo, ou a
+	estratégia para efetuar a operação em questão. As estratégias estão todas definidas previamente.
+*/
+
 public class Client {
 
 	public static void main(String[] args) {
